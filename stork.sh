2deebb99876c0a9618cf11ef57df0aabef191b1d
Andrew <healeycodes@gmail.com>
2021-09-16 14:08:39 +0100

    Add Stork v.1.2.1

--- stork.sh
+++ stork.sh
@@ -1,3 +1,2 @@
-wget https://files.stork-search.net/releases/latest/stork-ubuntu-16-04
-chmod +x stork-ubuntu-16-04
-./stork-ubuntu-16-04 build -i stork-posts.toml -o public/stork-posts.st
+chmod +x stork-ubuntu-16-04-v1.2.1
+./stork-ubuntu-16-04-v1.2.1 build -i stork-posts.toml -o public/stork-posts.st
