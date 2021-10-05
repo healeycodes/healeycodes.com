c037b6aa1ef83227c313ba32a535872fd63ae55a
James Little <littleguy23@gmail.com>
2021-10-05 15:48:20 -0400

    Use string literal instead of constant
    It's having trouble requiring the next/constants module from
    next.config.js, for some reason

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const { execSync } = require('child_process');
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase == PHASE_PRODUCTION_SERVER) {
+  if (phase == 'phase-production-server') {
     // Will run during production deploys and previews
     const storkStdout = execSync('./stork.sh')
+    console.log(storkStdout)
   }
   return defaultConfig
 }
