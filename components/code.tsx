a6d94e7b1f558d4728b857ff3a78a31e0af34679
Andrew <healeycodes@gmail.com>
2021-10-07 13:57:02 +0100

    Add niceware post

--- components/code.tsx
+++ components/code.tsx
@@ -1,3 +1,10 @@
+// Include additional languages here
+// --
+import Prism from "prism-react-renderer/prism";
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-rust")
+// --
+
 import Highlight, { defaultProps } from "prism-react-renderer";
 
 import codeTheme from "./codeTheme";
