559a14f725f878f11ec2206104625843146aac96
Andrew Healey <healeycodes@gmail.com>
2021-12-12 10:13:33 +0000

    Use flat.css

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -19,7 +19,7 @@ export default function Articles({ tags, posts }) {
   return (
     <Layout title={title} description="A list of every article I've written.">
       <Head>
-        <link rel="stylesheet" href="https://files.stork-search.net/basic.css" />
+        <link rel="stylesheet" href="https://files.stork-search.net/flat.css" />
       </Head>
       <h1 className="tag-desc">{title}</h1>
       <main>
@@ -33,11 +33,9 @@ export default function Articles({ tags, posts }) {
             ))
             .reduce((prev, curr) => [prev, ", ", curr])}
         </p>
-        <div className="stork">
-          <div className="stork-wrapper">
-            <input placeholder="Search all posts.." data-stork="posts" className="stork-input" />
-            <div data-stork="posts-output" className="stork-output"></div>
-          </div>
+        <div className="stork-wrapper">
+          <input placeholder="Search all posts.." data-stork="posts" className="stork-input" />
+          <div data-stork="posts-output" className="stork-output"></div>
         </div>
         <Script
           src="https://files.stork-search.net/stork.js"
@@ -47,7 +45,7 @@ export default function Articles({ tags, posts }) {
               'posts',
               'stork-posts.st'
             );
-            let elem: HTMLElement | null = document.querySelector('.stork')
+            let elem: HTMLElement | null = document.querySelector('.stork-wrapper')
             if (elem) {
               elem.style.visibility = 'visible'
             }
@@ -65,7 +63,7 @@ export default function Articles({ tags, posts }) {
           color: var(--light-text);
           padding-bottom: 24px;
         }
-        .stork {
+        .stork-wrapper {
           padding-bottom: 48px;
           visibility: hidden;
         }
@@ -74,7 +72,7 @@ export default function Articles({ tags, posts }) {
       {/* Ensure we don't show empty space for users without JS */}
       <noscript>
         <style>{`
-          .stork {
+          .stork-wrapper {
             display: none;
           }`}
         </style>
