cf46d2291d22c9f828852099f33c858174749201
Andrew <healeycodes@gmail.com>
2022-10-27 10:27:43 +0100

    Add vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import { GCScript } from "../components/gcScript";
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
 
     return <>
+        <Analytics />
         <GCScript siteUrl={"https://healeycodes.goatcounter.com/count"} />
         <Component {...pageProps} />
     </>
