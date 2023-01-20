2f5f316b2e1ef18fafcf66368542878c47d735eb
Andrew Healey <healeycodes@gmail.com>
2023-01-20 13:46:37 +0000

    Small /projects copy change

--- e2e/nav.spec.ts
+++ e2e/nav.spec.ts
@@ -6,6 +6,9 @@ test("nav bar links work", async ({ page }) => {
   // Navigate to `/articles`
   const articlesLink = await page.locator(`nav > ul > li > a[href="/articles"]`);
   await articlesLink.click()
+
+  // TODO: figure out why this is necessary
+  // I'm almost certain that having to wait means we're using the test framework wrong..
   await new Promise(r => setTimeout(r, 2000));
 
   // Title of `/articles`
