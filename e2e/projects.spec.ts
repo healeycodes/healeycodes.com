3558fc7606df5d49c8e859d5c8e91439d5724bd3
Andrew Healey <healeycodes@gmail.com>
2023-02-21 21:34:57 +0000

    Try and fix tests

--- e2e/projects.spec.ts
+++ e2e/projects.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from "@playwright/test";
 test("project page renders", async ({ page }) => {
   await page.goto("/projects");
 
+  // TODO: fix this – sleeping is an anti-pattern
+  await new Promise(r => setTimeout(r, 2000));
+
   // Intro visible
   const intro = await page.locator("text=My side projects include");
   expect(await intro.isVisible()).toBe(true);
