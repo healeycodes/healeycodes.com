3b84cd97cd18e84d03c8336ba91ab4cc54fd746e
Chever John <cheverjonathan@gmail.com>
2023-09-17 23:49:46 +0800

    feat: add recommdation post and remove initial post

--- data/posts.ts
+++ data/posts.ts
@@ -1,28 +1,9 @@
 // Based off analytics/HN ranking/gut feel
 export const popularPosts = [
-  "profiling-and-optimizing-an-interpreter",
-  "building-my-own-chess-engine",
-  "geoguessing-with-deep-learning",
+  "how-to-run-go-plugin-runner-in-apisix-ingress",
 ];
 
 // Good posts/highly viewed posts (not in any specific order)
 export const postStars = [
-  "running-untrusted-python-code",
-  "my-own-python-web-framework",
-  "sandboxing-javascript-code",
-  "profiling-and-optimizing-an-interpreter",
-  "designing-a-programming-language-for-advent-of-code",
-  "creating-the-golfcart-programming-language",
-  "implementing-bitcask-a-log-structured-hash-table",
-  "beating-grep-with-go",
-  "porting-niceware-to-rust",
-  "building-my-own-chess-engine",
-  "creating-randomness",
-  "geoguessing-with-deep-learning",
-  "virtual-ants",
-  "making-a-text-editor-with-a-game-engine",
-  "boids-flocking-simulation",
-  "webassembly-search-tools-for-static-websites",
-  "personal-websites-and-internet-writing",
   "majority-vote-algorithm",
 ];
