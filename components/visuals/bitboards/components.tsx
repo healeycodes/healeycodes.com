a753f03ff84883bad2896fe7146e23075d0f4a27
Andrew Healey <healeycodes@gmail.com>
2025-04-13 15:15:53 +0100

    avoid bigint literals

--- components/visuals/bitboards/components.tsx
+++ components/visuals/bitboards/components.tsx
@@ -12,11 +12,11 @@ const whitePawnAttacks: { codeLines: string[], steps: BitboardStep[] } = {
         "pawn_attacks  = attacks_left | attacks_right;",
     ],
     steps: (() => {
-        const FILE_A = 0x0101010101010101n
-        const FILE_H = 0x8080808080808080n
-        const white_pawns = 0x000000000000ff00n
-        const attacks_left = (white_pawns & ~FILE_A) << 7n
-        const attacks_right = (white_pawns & ~FILE_H) << 9n
+        const FILE_A = BigInt("0x0101010101010101")
+        const FILE_H = BigInt("0x8080808080808080")
+        const white_pawns = BigInt("0x000000000000ff00")
+        const attacks_left = (white_pawns & ~FILE_A) << BigInt(7)
+        const attacks_right = (white_pawns & ~FILE_H) << BigInt(9)
         const pawn_attacks = attacks_left | attacks_right
 
         return [
@@ -44,29 +44,29 @@ const knightAttack: { codeLines: string[], steps: BitboardStep[] } = {
         "  (KNIGHT_POS >> 6n & NOT_GH_FILE);",
     ],
     steps: (() => {
-        const KNIGHT_POS = 0x0000000004000000n
-        const NOT_AB_FILE = ~0x0303030303030303n  // Correct mask for files A and B
-        const NOT_GH_FILE = ~0xc0c0c0c0c0c0c0c0n  // Correct mask for files G and H
+        const KNIGHT_POS = BigInt("0x0000000004000000")
+        const NOT_AB_FILE = ~BigInt("0x0303030303030303")  // Correct mask for files A and B
+        const NOT_GH_FILE = ~BigInt("0xc0c0c0c0c0c0c0c0")  // Correct mask for files G and H
 
-        const attacks = (KNIGHT_POS << 17n) |
-                       (KNIGHT_POS << 15n) |
-                       (KNIGHT_POS << 10n & NOT_AB_FILE) |
-                       (KNIGHT_POS << 6n & NOT_GH_FILE) |
-                       (KNIGHT_POS >> 17n) |
-                       (KNIGHT_POS >> 15n) |
-                       (KNIGHT_POS >> 10n & NOT_AB_FILE) |
-                       (KNIGHT_POS >> 6n & NOT_GH_FILE)
+        const attacks = (KNIGHT_POS << BigInt(17)) |
+                       (KNIGHT_POS << BigInt(15)) |
+                       (KNIGHT_POS << BigInt(10) & NOT_AB_FILE) |
+                       (KNIGHT_POS << BigInt(6) & NOT_GH_FILE) |
+                       (KNIGHT_POS >> BigInt(17)) |
+                       (KNIGHT_POS >> BigInt(15)) |
+                       (KNIGHT_POS >> BigInt(10) & NOT_AB_FILE) |
+                       (KNIGHT_POS >> BigInt(6) & NOT_GH_FILE)
 
         return [
             { highlightLines: [2], board: KNIGHT_POS },
-            { highlightLines: [3], board: KNIGHT_POS << 17n },
-            { highlightLines: [4], board: KNIGHT_POS << 15n },
-            { highlightLines: [5], board: KNIGHT_POS << 10n & NOT_AB_FILE },
-            { highlightLines: [6], board: KNIGHT_POS << 6n & NOT_GH_FILE },
-            { highlightLines: [7], board: KNIGHT_POS >> 17n },
-            { highlightLines: [8], board: KNIGHT_POS >> 15n },
-            { highlightLines: [9], board: KNIGHT_POS >> 10n & NOT_AB_FILE },
-            { highlightLines: [10], board: KNIGHT_POS >> 6n & NOT_GH_FILE },
+            { highlightLines: [3], board: KNIGHT_POS << BigInt(17) },
+            { highlightLines: [4], board: KNIGHT_POS << BigInt(15) },
+            { highlightLines: [5], board: KNIGHT_POS << BigInt(10) & NOT_AB_FILE },
+            { highlightLines: [6], board: KNIGHT_POS << BigInt(6) & NOT_GH_FILE },
+            { highlightLines: [7], board: KNIGHT_POS >> BigInt(17) },
+            { highlightLines: [8], board: KNIGHT_POS >> BigInt(15) },
+            { highlightLines: [9], board: KNIGHT_POS >> BigInt(10) & NOT_AB_FILE },
+            { highlightLines: [10], board: KNIGHT_POS >> BigInt(6) & NOT_GH_FILE },
             { highlightLines: [3, 4, 5, 6, 7, 8, 9, 10], board: attacks },
         ]
     })(),
